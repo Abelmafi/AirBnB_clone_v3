2f5f0b11dabc7d95b0536924c2b10653464bc7ac
abelmesfin <abelmesfin555@gmail.com>
2023-01-19 23:59:08 +0300

    output error fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,14 +2,16 @@
 """Api creations"""
 
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown(error):
