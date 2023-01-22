19d210f55244a9e6f845b1a51bb6e3ce0d2eb6d6
abelmesfin <abelmesfin555@gmail.com>
2023-01-22 12:18:43 +0300

    commited

--- models/user.py
+++ models/user.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This is the user class"""
+import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -13,12 +14,25 @@ class User(BaseModel, Base):
         first_name: first name
         last_name: last name
     """
-    __tablename__ = 'users'
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
 
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
 
-    reviews = relationship('Review', cascade='all, delete', backref='user')
-    places = relationship('Place', cascade='all, delete', backref='user')
+        reviews = relationship('Review', cascade='all, delete', backref='user')
+        places = relationship('Place', cascade='all, delete', backref='user')
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
+        if self.password:
+            hashed_password = hashlib.md5(self.password.encode("utf-8"))
+            self.password = hashed_password.hexdigest()
